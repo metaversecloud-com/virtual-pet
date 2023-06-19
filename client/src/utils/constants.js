ff343a724d00495bb8142103dc3879b0a98356e5
Fabio Alexandrino <fabioalxk@users.noreply.github.com>
2023-06-19 17:50:42 -0300

    localhost 3001

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const SERVICE_HTTP_ADDRESS = "https://virtual-pet.topia-rtsdk.com:3001";
+export const SERVICE_HTTP_ADDRESS = "http://localhost:3001";
 
 // export const SERVICE_HTTP_ADDRESS = process.env.REACT_APP_API_URL
 //   ? process.env.REACT_APP_API_URL
