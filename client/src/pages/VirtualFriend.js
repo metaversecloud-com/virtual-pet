e12bd2ff3f3a0c2795a828e8334ce67688be9035
Fabio Alexandrino <fabioalxk@users.noreply.github.com>
2023-08-16 21:00:09 -0300

    Update VirtualFriend.js

--- client/src/pages/VirtualFriend.js
+++ client/src/pages/VirtualFriend.js
@@ -56,6 +56,7 @@ const VirtualFriend = () => {
   return (
     <div className="virtual-friend-wrapper">
       {getPetComponent()}
+
       {/* {visitor?.isAdmin && <MobileMenu />} */}
     </div>
   );
