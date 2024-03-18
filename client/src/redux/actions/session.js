ca7939048a46f826c6ca148d0faa408a0cee8db1
Fabio Alexandrino <fabioalxk@users.noreply.github.com>
2024-03-18 16:24:59 -0300

    Update session.js

--- client/src/redux/actions/session.js
+++ client/src/redux/actions/session.js
@@ -165,7 +165,6 @@ export const createPet = (petType, name) => async (dispatch) => {
 
 export const updatePet = (name, color) => async (dispatch) => {
   try {
-    console.log("hello");
     const queryParams = getQueryParams();
     const url = `/backend/pet?${queryParams}`;
 
